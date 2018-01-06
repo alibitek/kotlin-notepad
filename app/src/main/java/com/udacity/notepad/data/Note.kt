52357bdb3e8b975c5b3a0251ba65bbe7b11b28a9
Ali Bitek <alibitek@protonmail.ch>
2018-01-06 20:24:03 +0200

    Convert NoteDatabase to Kotlin

--- app/src/main/java/com/udacity/notepad/data/Note.kt
+++ app/src/main/java/com/udacity/notepad/data/Note.kt
@@ -4,7 +4,7 @@ import java.util.Date
 
 // data class provides = identity (equals), hashing (hashCode), copying (copy constructors)
 data class Note(
-        val id: Int = -1,
+        var id: Int = -1,
         var text: String? = null,
         var isPinned: Boolean = false,
         var createdAt: Date = Date(),
