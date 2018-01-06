2eb3243e53e94042d463f1e34a59e175dcf98e01
Ali Bitek <alibitek@protonmail.ch>
2018-01-06 19:46:53 +0200

    Add note about data class features

--- app/src/main/java/com/udacity/notepad/data/Note.kt
+++ app/src/main/java/com/udacity/notepad/data/Note.kt
@@ -2,7 +2,7 @@ package com.udacity.notepad.data
 
 import java.util.Date
 
-// data class provides = identity, hashing, copying
+// data class provides = identity (equals), hashing (hashCode), copying (copy constructors)
 data class Note(
         val id: Int = -1,
         var text: String? = null,
